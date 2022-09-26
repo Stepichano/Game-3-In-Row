fb649f0cb3c80a084f33964e6bef7bef820dc57e
MAKS <step10633@gmail.com>
2022-09-26 11:24:33 +0700

    Board

---
+++ WinFormsApp1/CircleButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+   public class CircleButton : Button
+    {
+        public Dictionary<int, Color> colors_button = new Dictionary<int, Color>()
+        {
+            { 0, Color.Red },
+            { 1, Color.Green },
+            { 2, Color.Blue },
+            { 3, Color.Yellow},
+            { 4, Color.Violet},
+
+        };
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(e);
+        }
+
+        
+    }
+
+}
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,10 +1,50 @@
+using System;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        public const int WHIDTH = 6;
+        public const int HEIGHT = 8;
+        CircleButton[,] array = new CircleButton[WHIDTH, HEIGHT]; 
+
         public Form1()
         {
             InitializeComponent();
+            Create_Board();
         }
+
+        private void Create_Board()
+        {
+            for (int i = 0; i < WHIDTH; i++)
+            {
+                for (int j = 0; j < HEIGHT; j++)
+                {   
+                    CircleButton bufer_button = Create_Button(i, j);
+                    this.Controls.Add(bufer_button);
+                    array [i, j] = bufer_button;
+                }
+            }
+
+        }
+
+        public CircleButton Create_Button(int i, int j)
+        {   Random rand = new Random();
+            CircleButton button = new CircleButton();
+            button.Width = 45;
+            button.Height = 45;
+            button.Left = 200 + 45 * i;
+            button.Top = 50 + 45 * j;
+            button.BackColor = button.colors_button[rand.Next(0, 5)];
+            return button;
+        }
+
+    
     }
+
+
 }
